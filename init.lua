1cb3d4b0509c5998d248b93eb50bfafe0663dfb8
eish238 <hieudoan238@gmail.com>
2024-07-03 14:14:38 -0400

    B: built v0.1_2024_07_03

--- init.lua
+++ init.lua
@@ -1,1 +1,2 @@
 require("core.mappings")
+require("core.plugins")
--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -1,2 +1,2 @@
 vim.g.mapleader = " " -- easy to reach leader key
-vim.keymap.set("n", "-", vim.cmd.Ex) -- need nvim 0.8+
+vim.keymap.set("n", "-", vim.cmd.Ex) -- need nvim 0.8
